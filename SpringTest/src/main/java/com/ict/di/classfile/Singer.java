799b581461533a38ae766f6bde879c70b9b0d5c5
nori-do <98159110+nori-do@users.noreply.github.com>
2022-05-17 20:53:20 +0900

    20220517

---
+++ SpringTest/src/main/java/com/ict/di/classfile/Singer.java
@@ -0,0 +1,11 @@
+package com.ict.di.classfile;
+
+public class Singer {
+	
+	// 가수는 무대가 있건 없건 노래를 할 수 있기 때문에
+	// 다른 어떠한 요소도 없이 오직 노래기능만 넣어둡니다.
+	
+	public void sing() {
+		System.out.println("가수가 노래를 합니다.");
+	}
+}
---
+++ SpringTest/src/main/java/com/ict/domain/test.java
@@ -0,0 +1,11 @@
+package com.ict.domain;
+
+import lombok.Data;
+
+@Data
+public class test {
+
+	private int a;
+	private String b;
+	private double c;
+}
